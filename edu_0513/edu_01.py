58725da6cea88b08df49dee198c2f2d6dd95a280
sipeporez <sipeporez@gmail.com>
2024-05-13 10:05:15 +0900

    python

--- edu_0513/edu_01.py
+++ edu_0513/edu_01.py
@@ -1,4 +1,22 @@
 data = [1, 2, 3]
 
 for d in data:
-    print(d)
+    print(d, end=" ")
+
+string = "hello"
+
+for s in string:
+    print(s, end=" ")
+
+
+data = (4, 5, 6)
+for i in data:
+    print(i)
+
+data2 = [i**2 for i in data]
+print(data2, type(data2))
+
+data3 = []
+for i in data:
+    data3.append(i**2)
+print(data3)
